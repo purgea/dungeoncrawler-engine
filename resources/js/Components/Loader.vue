31c1daf9106ddfb9853cfe85dee387f082c3951f
purgea <srdjanradovic0@gmail.com>
2026-07-29 12:47:24 +0200

    Fix loader

--- resources/js/Components/Loader.vue
+++ resources/js/Components/Loader.vue
@@ -31,33 +31,25 @@ defineExpose({
 <template>
     <div
         v-if="isVisible"
-        class="absolute inset-0 z-50 flex items-center justify-center bg-[#050604] text-[#ece7d8]"
+        class="absolute inset-0 z-50 flex items-center justify-center bg-[#090806] text-[#e6d7a4]"
         role="status"
         aria-live="polite"
         @click.stop
     >
         <div class="flex w-[min(86vw,360px)] flex-col items-center text-center">
-            <div class="relative h-16 w-16">
-                <div class="absolute inset-0 rotate-45 border border-[#817965]" />
-                <div
-                    v-if="!error"
-                    class="absolute inset-2 animate-spin border border-[#d8c98e] border-r-transparent"
-                />
-                <div
-                    v-else
-                    class="absolute inset-2 flex rotate-45 items-center justify-center border border-red-400/70 text-xl text-red-300"
-                >
-                    <span class="-rotate-45">!</span>
-                </div>
+            <div
+                v-if="!error"
+                class="h-[30px] w-[30px] animate-spin border-4 border-[#594a31] border-r-[#e0bd68] border-t-[#e0bd68] [animation-timing-function:steps(8,end)]"
+                aria-hidden="true"
+            >
+                <span />
             </div>
-            <div class="mt-6 text-sm font-bold uppercase tracking-[0.24em]">
+            <div v-else class="flex h-[46px] w-[46px] items-center justify-center border-2 border-[#b65d43] text-[28px] font-bold leading-[42px] text-[#d98261]" aria-hidden="true">!</div>
+            <div class="mt-5 text-sm font-bold uppercase tracking-[0.16em]">
                 {{ message }}
             </div>
             <div v-if="error" class="mt-3 text-xs leading-5 text-red-300">
                 {{ error }}
-            </div>
-            <div v-else class="mt-3 text-xs uppercase tracking-[0.16em] text-[#928b78]">
-                Carving the depths
             </div>
         </div>
     </div>
